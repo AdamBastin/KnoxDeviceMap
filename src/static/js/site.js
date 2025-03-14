3561fe6f39525503d422996ad703c92f4a7a476b
AdamBastin <adambastin98@gmail.com>
2025-03-14 12:36:09 -0400

    Added hacky way to get count of devices

--- src/static/js/site.js
+++ src/static/js/site.js
@@ -21,10 +21,25 @@ function refreshMap() {
     
 };
 
+async function getPlottedDevices(){
+    var count = "Loading...";
+    document.getElementById("total-devices-count").innerHTML = "Loading...";
+    try{
+        count = document.querySelectorAll('#map .awesome-marker').length / 2;
+    }catch{
+        count = -1;
+    }
+    document.getElementById("total-devices-count").innerHTML = count;
+}
+
 function hideLoaders(){
     document.getElementById("loader-container").style.display = 'none';
 }
 
 function showLoaders(){
     document.getElementById("loader-container").style.display = 'flex';
+}
+
+window.onload = function() {
+    getPlottedDevices();
 }
